4587289ef6cc823c660a1306a33ee88decef819c
tamirlanm <tamirlanmirgali@gmail.com>
2026-06-25 02:50:39 +0500

    add new DTOs jwtconfig and interfaces

--- backend/GreenVendor.Application/DTOs/RegisterRequest.cs
+++ backend/GreenVendor.Application/DTOs/RegisterRequest.cs
@@ -5,4 +5,5 @@ using GreenVendor.Domain.Enums;
 public class RegisterRequest{
     public string Email {get;set;} = string.Empty;
     public string Password {get;set;} = string.Empty;
+    public string Role {get;set;} = "Buyer";
 }
