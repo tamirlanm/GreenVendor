52788a5642d0c5d1dde7b1169f1d02518f9847c2
My2Profile <timkameiramkhan48@gmail.com>
2026-06-30 13:27:34 +0500

    first frontend mock commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+/// <reference types="react" />
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
