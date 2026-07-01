5f94f7011378813cc21b023adb1d225e25866590
tamirlanm <tamirlanmirgali@gmail.com>
2026-07-01 16:58:39 +0500

    add validator for put method

--- backend/GreenVendor.Application/Validators/UpdateSupplierValidator.cs
+++ backend/GreenVendor.Application/Validators/UpdateSupplierValidator.cs
@@ -1,7 +1,6 @@
 using GreenVendor.Application.DTOs;
 using FluentValidation;
 using GreenVendor.Application.Interfaces;
-using System.Data;
 using GreenVendor.Domain.Enums;
 namespace GreenVendor.Application.Validators;
 public class UpdateSupplierValidator : AbstractValidator<UpdateSupplierRequest>
@@ -10,18 +9,18 @@ public class UpdateSupplierValidator : AbstractValidator<UpdateSupplierRequest>
     public UpdateSupplierValidator(IAppDbContext db)
     {
         _db = db;
-        RuleFor(x => x.CompanyName).NotEmpty().WithMessage("Name required.").MaximumLength(150);
-        
+        RuleFor(x => x.CompanyName).NotEmpty().WithMessage("Company name must be a required field.").MaximumLength(100);
+
         RuleFor(x => x.Industry).NotEmpty().WithMessage("Industry cannot be empty.")
             .Must(industry => !industry.All(char.IsDigit) && Enum.TryParse<Industry>(industry, true, out _))
             .WithMessage("Invalid industry value. Please check for typos.");
 
         RuleFor(x => x.Description)
-            .MaximumLength(50)
+            .MinimumLength(50).MaximumLength(800)
             .When(UpdateSupplierRequest => !string.IsNullOrWhiteSpace(UpdateSupplierRequest.Description));;
         
         RuleFor(x => x.Email).NotEmpty().WithMessage("Email must be a required field.")
-            .EmailAddress().WithMessage("Incorrect email address format entered.")
+            .EmailAddress().WithMessage("Incorrect email address format entered.")  
             .MaximumLength(256).WithMessage("The length of the Email must not exceed 256 characters.");
         
         RuleFor(x => x.Phone)
